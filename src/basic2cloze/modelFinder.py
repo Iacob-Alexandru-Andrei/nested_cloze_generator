40f4fc2994e576b01a6bf85ee16dc5baeabd3398
RisingOrange <jakub.fidler@protonmail.com>
2021-09-29 18:35:56 +0200

    Fix error

--- src/basic2cloze/modelFinder.py
+++ src/basic2cloze/modelFinder.py
@@ -8,18 +8,24 @@ _basic_note_type_ids = []
 _cloze_note_type_ids = []
 
 
+def model_ids_for_names(names):
+    return [id for id in [mw.col.models.id_for_name(name) for name in names if name] if id]
+
+
 def get_models():
     """Prepare note type"""
     global _basic_note_type_ids
     global _cloze_note_type_ids
 
     if ANKI_VERSION_TUPLE >= (2, 1, 45):
-        _basic_note_type_ids = [mw.col.models.by_name(x)['id'] for x in ["Basic", tr.notetypes_basic_name()] if x]
-        _cloze_note_type_ids = [mw.col.models.by_name(x)['id'] for x in ["Cloze", tr.notetypes_cloze_name()] if x]
+        _basic_note_type_ids = model_ids_for_names(
+            ["Basic", tr.notetypes_basic_name()])
+        _cloze_note_type_ids = model_ids_for_names(
+            ["Cloze", tr.notetypes_cloze_name()])
     else:
         from anki.lang import _
-        _basic_note_type_ids = [mw.col.models.by_name(x)['id'] for x in ["Basic", _("Basic")] if x]
-        _cloze_note_type_ids = [mw.col.models.by_name(x)['id'] for x in ["Cloze", _("Cloze")] if x]
+        _basic_note_type_ids = model_ids_for_names(["Basic", _("Basic")])
+        _cloze_note_type_ids = model_ids_for_names(["Cloze", _("Cloze")])
 
     if not _basic_note_type_ids:
         tooltip("[Automatic Basic to Cloze] Cannot find source 'Basic' model")

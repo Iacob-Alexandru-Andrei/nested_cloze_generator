ea4d7db05e57b50b3e8d96ad48febb44713f200f
Iacob-Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2025-08-02 22:57:19 +0100

    Update prompts

--- src/nested_clz_generator/consts.py
+++ src/nested_clz_generator/consts.py
@@ -1,3 +1,3 @@
 from anki import version as anki_version  # type: ignore
 
-ANKI_VERSION_TUPLE = tuple(int(i) for i in anki_version.split("."))
+ANKI_VERSION_TUPLE = tuple(int(i.strip("b4")) for i in anki_version.split("."))
